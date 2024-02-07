dc6f0852495c8e88dcbbcdad2b26e9c60061e62c
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-07 17:27:08 +1300

    Initial Navbar component added, styling applied

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,17 @@
+import CollapsibleExample from './Navbar'
+
 function App() {
   return (
     <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
+      <CollapsibleExample />
+      {/* <img
+        src="client/styles/images/barbershop-logo.png"
+        width="500px"
+        height="auto"
+        alt=""
+      /> */}
+      {/* <h1>App</h1>
+      <p>React development has begun!</p> */}
     </div>
   )
 }
---
+++ client/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+function CollapsibleExample() {
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      className="bg-body-tertiary justify-content-center"
+      // data-bs-theme="dark"
+    >
+      <Container className="container-lg">
+        <Navbar.Brand href="#home">
+          <img
+            src="client/styles/images/barber-logo.png"
+            width="65"
+            height="65"
+            className="d-inline-block align-top"
+            alt="Barber logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Brand href="#home">Fresh & Faded</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <Nav.Link className="nav-link mx-lg-2" href="#about">
+              About
+            </Nav.Link>
+            <Nav.Link className="nav-link mx-lg-2" href="#gallery">
+              Gallery
+            </Nav.Link>
+            <Nav.Link className="nav-link mx-lg-2" href="#booking">
+              Booking
+            </Nav.Link>
+            <Nav.Link className="nav-link mx-lg-2" href="#contact">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default CollapsibleExample
