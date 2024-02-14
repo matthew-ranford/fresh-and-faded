5816af5b88920ae3645362bc5f10960793e799fc
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-14 20:29:06 +1300

    Added links to footer section

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -8,17 +8,31 @@ function Footer() {
       <Row className="text-center justify-content-center pb-3">
         <Col xs={2}>
           {/* <span className="input-group-text"> */}
-          <i className="bi bi-facebook display-5"></i>
+          <a
+            href="https://www.facebook.com/FreshandFadedBarberShop"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bi bi-facebook display-5"></i>
+          </a>
           {/* </span> */}
         </Col>
         <Col xs={2}>
           {/* <span className="input-group-text"> */}
-          <i className="bi bi-instagram display-5"></i>
+          <a
+            href="https://www.instagram.com/freshandfaded17/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bi bi-instagram display-5"></i>
+          </a>
           {/* </span> */}
         </Col>
         <Col xs={2}>
           {/* <span className="input-group-text"> */}
-          <i className="bi bi-telephone display-5"></i>
+          <a href="tel: 021 243 7374">
+            <i className="bi bi-telephone display-5"></i>
+          </a>
           {/* </span> */}
         </Col>
       </Row>
