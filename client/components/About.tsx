79a41ae2751e6215297bf72c7bad3e60b1525203
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-17 12:29:48 +1300

    Initial framer motion declarations added About component

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 function About() {
   return (
     <>
@@ -5,29 +7,38 @@ function About() {
         <div className="container-lg ">
           <div className="row justify-content-center align-items-center">
             <div className="col-md-7 d-md-block">
-              <img
+              <motion.img
+                initial={{ x: -100 }}
+                transition={{ ease: 'linear', duration: 1.5 }}
+                animate={{ x: 0 }}
                 className="img-fluid"
                 src="client/styles/images/barbershop-logo.png"
                 alt="Barbershop card"
               />
             </div>
             <div className="col-md-5 text-center text-md-start">
-              <h1>
-                <div className="display-4 text-primary">
-                  Welcome to Fresh & Faded
-                </div>
-                <div className="display-5 text-secondary p">Barbershop</div>
-              </h1>
-              <p className="lead my-4 text-secondary">
-                Come in for a fresh cut, nice fade, beard trim or a cut throat
-                shave.
-              </p>
-              <p className="lead my-4 text-secondary">
-                Great barber! Great banter! Quality cuts!
-              </p>
-              <p className="lead my-4 text-secondary">
-                We like to make you feel good!
-              </p>
+              <motion.div
+                initial={{ x: 100 }}
+                transition={{ ease: 'linear', duration: 1.5 }}
+                animate={{ x: 0 }}
+              >
+                <h1>
+                  <div className="display-4 text-primary">
+                    Welcome to Fresh & Faded
+                  </div>
+                  <div className="display-5 text-secondary p">Barbershop</div>
+                </h1>
+                <p className="lead my-4 text-secondary">
+                  Come in for a fresh cut, nice fade, beard trim or a cut throat
+                  shave.
+                </p>
+                <p className="lead my-4 text-secondary">
+                  Great barber! Great banter! Quality cuts!
+                </p>
+                <p className="lead my-4 text-secondary">
+                  We like to make you feel good!
+                </p>
+              </motion.div>
             </div>
           </div>
         </div>
