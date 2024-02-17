2bd1563ef926d6dc7cad72fd0be6705b3cfad2da
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-17 13:18:12 +1300

    Trying out some things with framer motion in the App component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -5,16 +5,24 @@ import Contact from './Contact'
 import Location from './Location'
 import Footer from './Footer'
 
+import { motion } from 'framer-motion'
+
 function App() {
   return (
     <>
       {/* <div className="background"> */}
       <Navigation />
       <About />
-      <Gallery />
-      <Contact />
-      <Location />
-      <Footer />
+      <motion.div
+        initial={{ x: -500, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ ease: 'linear', duration: 1.5 }}
+      >
+        <Gallery />
+        <Contact />
+        <Location />
+        <Footer />
+      </motion.div>
       {/* </div> */}
     </>
   )
