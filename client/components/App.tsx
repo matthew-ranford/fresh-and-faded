4a992149f865618731c12390c04a67cf921e89a3
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-17 14:58:46 +1300

    Added hover effect to Location component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -10,20 +10,20 @@ import { motion } from 'framer-motion'
 function App() {
   return (
     <>
-      {/* <div className="background"> */}
-      <Navigation />
-      <About />
-      <motion.div
-        initial={{ x: -500, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }}
-        transition={{ ease: 'linear', duration: 2.5 }}
-      >
-        <Gallery />
-        <Contact />
-        <Location />
-        <Footer />
-      </motion.div>
-      {/* </div> */}
+      <div className="background">
+        <Navigation />
+        <About />
+        <motion.div
+          initial={{ x: -500, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ ease: 'linear', duration: 2.5 }}
+        >
+          <Gallery />
+          <Contact />
+          <Location />
+          <Footer />
+        </motion.div>
+      </div>
     </>
   )
 }
--- client/components/Location.tsx
+++ client/components/Location.tsx
@@ -1,4 +1,5 @@
 import ListGroup from 'react-bootstrap/ListGroup'
+import { motion } from 'framer-motion'
 
 function Location() {
   return (
@@ -7,7 +8,10 @@ function Location() {
         <div className="container-lg mb-5">
           <div className="row justify-content-center align-items-center">
             <div className="col-md-7 d-md-block">
-              <iframe
+              <motion.iframe
+                whileHover={{
+                  boxShadow: '0px 0px 20px rgb(255, 255, 255)',
+                }}
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2915.496136760547!2d174.91503366063242!3d-41.223369735726585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38aa43fdc7206d%3A0xc266558213600c39!2sFresh%20%26%20Faded%20BARBERSHOP!5e0!3m2!1sen!2snz!4v1707807960468!5m2!1sen!2snz"
                 width="600"
                 height="450"
@@ -15,7 +19,7 @@ function Location() {
                 style={{ border: 0 }}
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
+              ></motion.iframe>
             </div>
             <div className="col-md-5 text-center text-md-start">
               <h1>
