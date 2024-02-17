6cf3b91b28c2bff2367fd63d696994e5a9b58319
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-17 16:11:00 +1300

    Refactored code to use variant type

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -3,6 +3,15 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { motion } from 'framer-motion'
 
+const containerVariants = {
+  hidden: {
+    y: -500,
+  },
+  visible: {
+    y: 0,
+  },
+}
+
 function Navigation() {
   return (
     <motion.div
@@ -14,12 +23,13 @@ function Navigation() {
         <Container className="container-lg">
           <Navbar.Brand href="#home">
             <motion.img
-              initial={{ y: -500 }}
+              variants={containerVariants}
+              initial="hidden"
               transition={{
                 ease: 'linear',
                 duration: 2,
               }}
-              animate={{ y: 0 }}
+              animate="visible"
               src="https://scontent.fpmr1-1.fna.fbcdn.net/v/t1.18169-9/15698060_222415818208286_2851917130035870513_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=7a1959&_nc_ohc=IrQt0O2YF5AAX_aBSgV&_nc_ht=scontent.fpmr1-1.fna&oh=00_AfBbRPOveThPJeJK975YRvu5igMi8wv1rDPvJllXGljQ2g&oe=65ECF1B3"
               width="65"
               height="65"
@@ -29,12 +39,13 @@ function Navigation() {
           </Navbar.Brand>
           <Navbar.Brand href="#home">
             <motion.div
-              initial={{ y: -500 }}
+              variants={containerVariants}
+              initial="hidden"
               transition={{
                 ease: 'linear',
                 duration: 2,
               }}
-              animate={{ y: 0 }}
+              animate="visible"
             >
               Fresh & Faded
             </motion.div>
@@ -44,12 +55,13 @@ function Navigation() {
             <Nav className="navbar-nav justify-content-end flex-grow-1 pe-3">
               <Nav.Link className="nav-link mx-lg-2" href="#about">
                 <motion.div
-                  initial={{ y: -500 }}
+                  variants={containerVariants}
+                  initial="hidden"
                   transition={{
                     ease: 'linear',
                     duration: 2,
                   }}
-                  animate={{ y: 0 }}
+                  animate="visible"
                   whileHover={{ color: 'rgb(139, 23, 23)', transition: 1 }}
                 >
                   About
@@ -57,12 +69,13 @@ function Navigation() {
               </Nav.Link>
               <Nav.Link className="nav-link mx-lg-2" href="#gallery">
                 <motion.div
-                  initial={{ y: -500 }}
+                  variants={containerVariants}
+                  initial="hidden"
                   transition={{
                     ease: 'linear',
                     duration: 2,
                   }}
-                  animate={{ y: 0 }}
+                  animate="visible"
                   whileHover={{ color: 'rgb(139, 23, 23)', transition: 1 }}
                 >
                   Gallery
@@ -70,12 +83,13 @@ function Navigation() {
               </Nav.Link>
               <Nav.Link className="nav-link mx-lg-2" href="#booking">
                 <motion.div
-                  initial={{ y: -500 }}
+                  variants={containerVariants}
+                  initial="hidden"
                   transition={{
                     ease: 'linear',
                     duration: 2,
                   }}
-                  animate={{ y: 0 }}
+                  animate="visible"
                   whileHover={{ color: 'rgb(139, 23, 23)', transition: 1 }}
                 >
                   Booking
@@ -83,12 +97,13 @@ function Navigation() {
               </Nav.Link>
               <Nav.Link className="nav-link mx-lg-2" href="#contact">
                 <motion.div
-                  initial={{ y: -500 }}
+                  variants={containerVariants}
+                  initial="hidden"
                   transition={{
                     ease: 'linear',
                     duration: 2,
                   }}
-                  animate={{ y: 0 }}
+                  animate="visible"
                   whileHover={{ color: 'rgb(139, 23, 23)', transition: 1 }}
                 >
                   Contact
