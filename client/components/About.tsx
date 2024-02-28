52a8715fdf437330b06bb09beecdca44115f4c59
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-28 15:50:59 +1300

    Further adjustments to styling

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -63,79 +63,98 @@ function About() {
                     src="client/styles/images/barbershop-hero2.png"
                     alt="Barbershop card"
                   />
-                </motion.div>
-                <p
-                  className="my-4 text-center"
-                  style={{ color: 'rgb(255, 255, 255)' }}
-                >
-                  <motion.div
-                    className="font-change"
-                    initial={{ opacity: 0, y: 50 }}
-                    transition={{ duration: 1 }}
-                    whileInView={{ y: 0, opacity: 1 }}
+                  <p
+                    className="my-4 text-center"
+                    style={{ color: 'rgb(255, 255, 255)' }}
                   >
-                    Keeping the mature man fresh & the young man faded
-                  </motion.div>
-                </p>
+                    <motion.div
+                      className="font-change"
+                      initial={{ opacity: 0, y: 50 }}
+                      transition={{ duration: 1 }}
+                      whileInView={{ y: 0, opacity: 1 }}
+                    >
+                      Keeping the mature man fresh & the young man faded
+                    </motion.div>
+                  </p>
+                  <p
+                    className="my-4 text-center"
+                    style={{ color: 'rgb(180, 34, 34)' }}
+                  >
+                    <motion.div
+                      className="font-change"
+                      initial={{ opacity: 0, y: 50 }}
+                      transition={{ duration: 1 }}
+                      whileInView={{ y: 0, opacity: 1 }}
+                    >
+                      Since 2017
+                    </motion.div>
+                  </p>
+                </motion.div>
               </div>
               <div className="col-md-6 pt-5 mt-5 px-5">
-                <div className="pb-3">
-                  <motion.div
-                    initial={{ opacity: 0, x: 50 }}
-                    transition={{ duration: 1 }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                  >
-                    <p
-                      className="lead my-4"
-                      style={{ color: 'rgb(255, 255, 255)' }}
+                <motion.div
+                  initial={{ x: 100, opacity: 0 }}
+                  transition={{ ease: 'linear', duration: 2.5 }}
+                  animate={{ x: 0, opacity: 1 }}
+                >
+                  <div className="pb-3">
+                    <motion.div
+                      initial={{ opacity: 0, x: 50 }}
+                      transition={{ duration: 1 }}
+                      whileInView={{ x: 0, opacity: 1 }}
                     >
-                      Come in for a fresh cut, nice fade, beard trim or a cut
-                      throat shave.
-                    </p>
-                  </motion.div>
-                  <motion.div
-                    initial={{ opacity: 0, x: -50 }}
-                    transition={{ duration: 1 }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                  >
-                    <p
-                      className="lead my-4"
-                      style={{ color: 'rgb(255, 255, 255)' }}
+                      <p
+                        className="lead my-4"
+                        style={{ color: 'rgb(255, 255, 255)' }}
+                      >
+                        Come in for a fresh cut, nice fade, beard trim or a cut
+                        throat shave!
+                      </p>
+                    </motion.div>
+                    <motion.div
+                      initial={{ opacity: 0, x: -50 }}
+                      transition={{ duration: 1 }}
+                      whileInView={{ x: 0, opacity: 1 }}
                     >
-                      Great barber! Great banter! Quality cuts!
-                    </p>
-                  </motion.div>
-                  <motion.div
-                    initial={{ opacity: 0, x: 50 }}
-                    transition={{ duration: 1 }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                  >
-                    <p
-                      className="lead my-4"
-                      style={{ color: 'rgb(255, 255, 255)' }}
+                      <p
+                        className="lead my-4"
+                        style={{ color: 'rgb(255, 255, 255)' }}
+                      >
+                        Great barber! Great banter! Quality cuts!
+                      </p>
+                    </motion.div>
+                    <motion.div
+                      initial={{ opacity: 0, x: 50 }}
+                      transition={{ duration: 1 }}
+                      whileInView={{ x: 0, opacity: 1 }}
                     >
-                      We like to make you feel good!
-                    </p>
-                  </motion.div>
-                  <motion.a
-                    href="#hours"
-                    className="btn btn-primary px-3 py-2 mt-2"
-                    style={{
-                      background: '#272727',
-                      color: 'white',
-                      borderColor: 'white',
-                    }}
-                    aria-controls="example-collapse-text"
-                    aria-expanded={open}
-                    variants={buttonHoverVariant}
-                    whileHover="hover"
-                    initial={{ opacity: 0, y: 100 }}
-                    transition={{ duration: 0.8 }}
-                    whileInView={{ y: 0, opacity: 1 }}
-                  >
-                    Barbershop Hours!
-                  </motion.a>
-                </div>
+                      <p
+                        className="lead my-4"
+                        style={{ color: 'rgb(255, 255, 255)' }}
+                      >
+                        We like to make you feel good!
+                      </p>
+                    </motion.div>
+                    <motion.a
+                      href="#hours"
+                      className="btn btn-primary px-3 py-2 mt-2"
+                      style={{
+                        background: '#272727',
+                        color: 'white',
+                        borderColor: 'white',
+                      }}
+                      aria-controls="example-collapse-text"
+                      aria-expanded={open}
+                      variants={buttonHoverVariant}
+                      whileHover="hover"
+                      initial={{ opacity: 0, y: 100 }}
+                      transition={{ duration: 0.8 }}
+                      whileInView={{ y: 0, opacity: 1 }}
+                    >
+                      Barbershop Hours!
+                    </motion.a>
+                  </div>
+                </motion.div>
               </div>
             </div>
           </div>
