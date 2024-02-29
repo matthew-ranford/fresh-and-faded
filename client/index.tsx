5e30b52482d2655a99bb8e4b3f26b3c7846622fe
Matthew Ranford <matt.ranford16@gmail.com>
2024-02-29 20:40:40 +1300

    Initial setup of sass file structure, testing custom variable appears to be working

--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '/client/css/main.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './components/App.tsx'
